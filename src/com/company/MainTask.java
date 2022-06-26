ded237ea429561a5108b41911e3c32d28a71f56c
Darya Zagurskaya <ogurchikdasha@gmail.com>
2022-06-26 19:30:45 +0300

    The fifth - done

--- src/com/company/MainTask.java
+++ src/com/company/MainTask.java
@@ -37,7 +37,7 @@ public class MainTask {
             System.out.print(array[i] + " ");
         }
     }*/
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the num");
         int [] array = new int [3];
@@ -49,5 +49,49 @@ public class MainTask {
             sum += num;
         }
         System.out.println(sum);
+    }*/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write the number 1-12");
+        switch (scanner.nextInt()){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Please, try again");
+        }
     }
 }
