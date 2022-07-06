d88e9a0079a35626f8b61a1e79f46df4c1880360
Darya Zagurskaya <ogurchikdasha@gmail.com>
2022-07-06 15:48:20 +0300

    Task 2 - done

--- src/com/company/Basics_of_software_code_development/Task2.java
+++ src/com/company/Basics_of_software_code_development/Task2.java
@@ -1,4 +1,22 @@
 package com.company.Basics_of_software_code_development;
 
 public class Task2 {
+    public static void main(String[] args) {
+        double a = 3;
+        double sqrt;
+        double b = 5;
+        double c = 7;
+        double result;
+        double powB;
+        double powBm;
+        double powA;
+      //  System.out.println(result);
+        powB = Math.pow(b,2);
+        powBm = Math.pow(b,-2);
+        sqrt = Math.sqrt(powB + 4*a*c);
+        powA = Math.pow(a,3);
+        System.out.println(powB + " " + powBm + " " + sqrt + " " + powA);
+        result = ((b+sqrt)/(2*a) - ((powA * c) + powBm));
+        System.out.println(result);
+    }
 }
