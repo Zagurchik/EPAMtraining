708550581586ce1aee422c1d49afbc51177ce7c8
Darya Zagurskaya <ogurchikdasha@gmail.com>
2022-07-06 17:20:38 +0300

    Task 6 - done

---
+++ src/com/company/Basics_of_software_code_development/Task6.java
@@ -0,0 +1,23 @@
+package com.company.Basics_of_software_code_development;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Task6 task6 = new Task6();
+        System.out.println(task6.aBoolean(-3,2));
+    }
+
+    public boolean aBoolean(int x, int y) {
+        boolean b = false;
+        if (x <= 2 && y <= 4) {
+            if (x <= 4 && y >= -3) {
+                if (x >= -2 && y <= 4) {
+                    if (x >= -4 && y >= -3){
+                        b = true;
+                    }
+                }
+            }
+        }
+        return b;
+
+    }
+}
