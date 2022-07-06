c0ea92edf0077383b3d1c3c3a08cf12125207d76
Darya Zagurskaya <ogurchikdasha@gmail.com>
2022-07-06 22:16:13 +0300

    Task 5 - fixed

--- src/com/company/Basics_of_software_code_development/Task5.java
+++ src/com/company/Basics_of_software_code_development/Task5.java
@@ -6,14 +6,15 @@ package com.company.Basics_of_software_code_development;
 public class Task5 {
     public static void main(String[] args) {
         Task5 time = new Task5();
-        int sec = 12012;
+        int sec = 3_447_328;
         System.out.println(time.clock(sec));
     }
 
     public  String clock(int second) {
         int min = second / 60;
         int hours = min / 60;
-        String time = hours + "ч " + min + "мин " + second + "с";
+        int second1 = second % 60 ;
+        String time = hours + "ч " + min % 60 + "мин " + second1 + "с";
         return time;
     }
 }
