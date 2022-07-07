27208b88ff360c98f35903564a44ae715ad77dd4
Darya Zagurskaya <ogurchikdasha@gmail.com>
2022-07-07 13:33:06 +0300

    Task5 - done

--- src/com/company/Basics_of_software_code_development/Сycles/Task5.java
+++ src/com/company/Basics_of_software_code_development/Сycles/Task5.java
@@ -1,5 +1,5 @@
 package com.company.Basics_of_software_code_development.Сycles;
-
+// Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
 public class Task5 {
     public static void main(String[] args) {
                 System.out.print("ASCII символы и коды:\n № символ OCT  HEX\n");
